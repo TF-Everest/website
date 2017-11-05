2dd47c299f0f2caecab0a8af73a69e889de1c155
Alexander Christie <alex@briefly.io>
2017-11-05 16:57:16 +0000

    Add Google Analytics to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,12 @@ module.exports = {
   },
   plugins: [
       `gatsby-plugin-react-helmet`,
-      `gatsby-plugin-sass`
+      `gatsby-plugin-sass`,
+      {
+	    resolve: `gatsby-plugin-google-analytics`,
+	    options: {
+	      trackingId: 'UA-109264620-1'
+	    }
+	  }
   ],
 }
